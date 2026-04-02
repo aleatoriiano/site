8f88789731fcf84d4692e093053329910361cd8d
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-04-02 18:57:29 -0300

    Update artilheiros.js

--- artilheiros.js
+++ artilheiros.js
@@ -10,4 +10,11 @@ const ARTILHEIROS = [
       "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/4cebeb6d3d6ccaab8326f14108d18e6e~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=7ebaeec6&x-expires=1775318400&x-signature=jo428cTi2JkqcKb9714hWRLQP2w%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my",
     teams: ["vasco.png"],
   },
+   {
+    name: "guizinxz",
+    level: 1,
+    foto:
+      "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/ca598c8bf34925c97eb29cec165f6d35~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=19bb10c9&x-expires=1775336400&x-signature=OKjv6yIL9aKPaKtxSfbfy0lVPe8%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=sg1",
+    teams: ["palmeiras.png"],
+  },
 ];
