731654521d65de6ed382c6fbc3cf5ff822af9e0e
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-04-02 23:59:39 -0300

    Update artilheiros.js

--- artilheiros.js
+++ artilheiros.js
@@ -17,4 +17,23 @@ const ARTILHEIROS = [
       "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/ca598c8bf34925c97eb29cec165f6d35~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=19bb10c9&x-expires=1775336400&x-signature=OKjv6yIL9aKPaKtxSfbfy0lVPe8%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=sg1",
     teams: ["palmeiras.png"],
   },
+     {
+    name: "🐺 jokerzin 🐼",
+    level: 1,
+    foto:
+      "https://p16-common-sign.tiktokcdn.com/tos-maliva-avt-0068/a30f373677caef0b64f19c9dfaf61ed7~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=2bad26d1&x-expires=1775354400&x-signature=CdKFSq8bS0mvWL2UvQ5nb121yRA%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=sg1",
+    teams: ["corinthians.png"],
+  },
+  {
+    name: "eo_christ🥷",
+    level: 1,
+    foto: "https://p16-common-sign.tiktokcdn.com/tos-alisg-avt-0068/68a06d13b4aab48ea2d23f85ec537151~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=3e177b03&x-expires=1775354400&x-signature=gGTtlDuAvCFjbpHwHif0fCudDbQ%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=sg1",
+    teams: ["corinthians.png"],
+   /* {
+    name: "",
+    level: 0,
+    foto:
+      "",
+    teams: [""],
+  }, */
 ];
