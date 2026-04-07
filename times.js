9ee9a2a12a749f570cedd2b2d4ee4b75a1cd46a6
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-04-07 19:05:36 -0300

    Update times.js

--- times.js
+++ times.js
@@ -14,7 +14,7 @@ const TEAMS = [
   { name: "Bahia", titles: 2, shield: "bahia.png" },
   { name: "Internacional", titles: 2, shield: "internacional.png" },
   { name: "Real Madrid", titles: 1, shield: "real-madrid.png" },
-  { name: "Botafogo", titles: 0, shield: "botafogo.png" },
+  { name: "Botafogo", titles: 1, shield: "botafogo.png" },
   { name: "Mirassol", titles: 2, shield: "mirassol.png" },
   { name: "Atlético Paranaense", titles: 0, shield: "athletico-pr.png" },
   { name: "Cruzeiro", titles: 1, shield: "cruzeiro.png" },
