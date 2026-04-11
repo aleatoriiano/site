2fc4f24772067287661615f4f10fd18b49135540
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-04-11 17:47:33 -0300

    Update artilheiros.js

--- artilheiros.js
+++ artilheiros.js
@@ -14,8 +14,7 @@ const ARTILHEIROS = [
   {
     name: "TATU PASSOS",
     level: 1,
-    foto:
-      "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/4cebeb6d3d6ccaab8326f14108d18e6e~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=7ebaeec6&x-expires=1775318400&x-signature=jo428cTi2JkqcKb9714hWRLQP2w%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my",
+    foto: "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/4cebeb6d3d6ccaab8326f14108d18e6e~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=7ebaeec6&x-expires=1775318400&x-signature=jo428cTi2JkqcKb9714hWRLQP2w%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my",
     teams: ["vasco.png"],
   },
    {
