750c5ad26fc411557f4b0a32c2cf6f89e9eb1dc6
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-04-13 18:13:26 -0300

    Update times.js

--- times.js
+++ times.js
@@ -6,7 +6,7 @@ const TEAMS = [
   { name: "Corinthians", titles: 7, shield: "corinthians.png" },
   { name: "Atlético Mineiro", titles: 3, shield: "atletico-mg.png" },
   { name: "Vasco", titles: 6, shield: "vasco.png" },
-  { name: "Grêmio", titles: 3, shield: "gremio.png" },
+  { name: "Grêmio", titles: 4, shield: "gremio.png" },
   { name: "Palmeiras", titles: 4, shield: "palmeiras.png" },
   { name: "Flamengo", titles: 12, shield: "flamengo.png" },
   { name: "Santos", titles: 5, shield: "santos.png" },
