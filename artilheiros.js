9315e79f868bc64a392c63e57db18204e47f4672
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-04-17 21:34:34 -0300

    Update artilheiros.js

--- artilheiros.js
+++ artilheiros.js
@@ -88,7 +88,7 @@ const ARTILHEIROS = [
    {
     name: "thzinn.pph7️⃣💤🇵🇹",
     level: 16,
-    foto: "https://p77-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/ee405d7e14ab67c6ab62e7b8bf21e673~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=5ad7e121&x-expires=1775527200&x-signature=0Qpfv8KVx3PbhTIrEQoG4qwQEIY%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my",
+    foto: "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/9f03a1a3765c63a7d2f421d5b596d683~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=8c6ee742&x-expires=1776643200&x-signature=QgBR1J5aI5DNj6Qf83f1TxFouRA%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my3",
     teams: ["benfica.png", "flamengo.png", "flamengo.png", "flamengo.png", "psg.png", "flamengo.png", "flamengo.png", "santos.png", "flamengo.png", "santos.png", "santos.png", "flamengo.png"],
   },
     {
