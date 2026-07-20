112b8ffa8b533f9f937e5c4b26cfa57c30020018
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-07-20 15:12:29 -0300

    Update times.js

--- times.js
+++ times.js
@@ -3,10 +3,10 @@
  * A ordem na tela é automática: mais títulos primeiro; em empate, ordem alfabética do nome.
  */
 const TEAMS = [
-  { name: "Corinthians", titles: 7, shield: "corinthians.png" },
+  { name: "Corinthians", titles: 8, shield: "corinthians.png" },
   { name: "Atlético Mineiro", titles: 0, shield: "atletico-mg.png" },
   { name: "Vasco", titles: 0, shield: "vasco.png" },
-  { name: "Grêmio", titles: 6, shield: "gremio.png" },
+  { name: "Grêmio", titles: 7, shield: "gremio.png" },
   { name: "Palmeiras", titles: 0, shield: "palmeiras.png" },
   { name: "Flamengo", titles: 3, shield: "flamengo.png" },
   { name: "Santos", titles: 0, shield: "santos.png" },
@@ -22,13 +22,10 @@ const TEAMS = [
   { name: "Coritiba", titles: 0, shield: "coritiba.png" },
   { name: "Chapecoense", titles: 0, shield: "chapecoense.png" },
   { name: "Sao Paulo", titles: 0, shield: "sao-paulo.png" },
-  { name: "Benfica", titles: 0, shield: "benfica.png" },
-  { name: "psg", titles: 0, shield: "psg.png" },
-  { name: "Manchester City", titles: 0, shield: "manchester-city.png" },
-   { name: "Sao Caetano", titles: 0, shield: "sao-caetano.png" },
   { name: "Portuguesa", titles: 1, shield: "portuguesa.png" },
   { name: "Avai", titles: 1, shield: "avai.png" },
   { name: "Ceara", titles: 1, shield: "ceara.png" },
   { name: "Treze", titles: 1, shield: "treze.png" },
 { name: "Cascavel", titles: 1, shield: "cascavel.png" },
+  { name: "Caxias", titles: 1, shield: "caxias.png" },
 ];
