f1d375d123d33158ad5ff6ad970d2f2d8ef74052
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-07-21 16:14:42 -0300

    Update artilheiros.js

--- artilheiros.js
+++ artilheiros.js
@@ -43,9 +43,9 @@ const ARTILHEIROS = [
   },
      {
     name: "voando_alto",
-    level: 13,
+    level: 18,
     foto: "https://p16-common-sign.tiktokcdn.com/tos-alisg-avt-0068/aa2de14b242dff8a4a4e2596e46613e9~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=539fe965&x-expires=1784577600&x-signature=0wfv9nR8oFlTiMTvGUW04GRzrNI%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my2",
-    teams: ["gremio.png", "cascavel.png", "gremio.png", "gremio.png", "gremio.png", "barcelona.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png"],
+    teams: ["gremio.png", "cascavel.png", "gremio.png", "gremio.png", "gremio.png", "barcelona.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png", "gremio.png"],
   },
    {
     name: "Gui_mct",
