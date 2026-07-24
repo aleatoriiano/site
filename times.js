9332755e89c96fbf99b94cef6d7b5190f65d774b
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-07-24 20:44:49 -0300

    Update times.js

--- times.js
+++ times.js
@@ -3,10 +3,10 @@
  * A ordem na tela é automática: mais títulos primeiro; em empate, ordem alfabética do nome.
  */
 const TEAMS = [
-  { name: "Corinthians", titles: 12, shield: "corinthians.png" },
-  { name: "Atlético Mineiro", titles: 4, shield: "atletico-mg.png" },
+  { name: "Corinthians", titles: 13, shield: "corinthians.png" },
+  { name: "Atlético Mineiro", titles: 5, shield: "atletico-mg.png" },
   { name: "Vasco", titles: 4, shield: "vasco.png" },
-  { name: "Grêmio", titles: 49, shield: "gremio.png" },
+  { name: "Grêmio", titles: 51, shield: "gremio.png" },
   { name: "Palmeiras", titles: 1, shield: "palmeiras.png" },
   { name: "Flamengo", titles: 5, shield: "flamengo.png" },
   { name: "Santos", titles: 0, shield: "santos.png" },
