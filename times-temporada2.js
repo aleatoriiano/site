0768a9353a7fa5099ff9ae22ffa330f916d371d6
aleatoriiano <harrisonmcalister.contato@hotmail.com>
2026-08-23 16:32:07 -0300

    Update times-temporada2.js

--- times-temporada2.js
+++ times-temporada2.js
@@ -4,7 +4,7 @@
  */
 window.TEAMS_BY_SEASON = window.TEAMS_BY_SEASON || {};
 window.TEAMS_BY_SEASON[2] = [
-  { name: "Gremio", titles: 38, shield: "gremio.png" },
+  { name: "Gremio", titles: 40, shield: "gremio.png" },
   { name: "Vitoria", titles: 4, shield: "vitoria.png" },
    { name: "Mirassol", titles: 1, shield: "mirassol.png" },
   { name: "Remo", titles: 0, shield: "remo.png" },
@@ -12,7 +12,7 @@ window.TEAMS_BY_SEASON[2] = [
   { name: "Flamengo", titles: 3, shield: "flamengo.png" },
   { name: "Ibis", titles: 1, shield: "ibis.png" },
   { name: "Fluminense", titles: 0, shield: "fluminense.png" },
-  { name: "Botafogo", titles: 1, shield: "botafogo.png" },
+  { name: "Botafogo", titles: 3, shield: "botafogo.png" },
   { name: "Bahia", titles: 0, shield: "bahia.png" },
   { name: "Aguia de Maraba", titles: 2, shield: "aguia-de-maraba.png" },
   { name: "Brasil de Pelotas", titles: 2, shield: "brasil-de-pelotas.png" },
